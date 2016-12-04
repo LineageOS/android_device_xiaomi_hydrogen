41ef2ebac1f90b571c73fef9b38579bbed1b6edd
TheScarastic <warabhishek@gmail.com>
2016-12-04 07:47:46 +0000

    hydrogen: Let's be unique!! Let's be a Hydrogen
    Change-Id: I2883ba66eece5ce86173def76dcb796eb71a9262

--- full_hydrogen.mk
+++ full_hydrogen.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from kipper device
+# Inherit from hydrogen device
 $(call inherit-product, device/xiaomi/hydrogen/device.mk)
 
 # Device identifier. This must come after all inclusions
